5e319a5580cc787bf8a7a46ca256243b1ad06f86
TIT-173 Siddhesh Patne <sidpatne102001@gmail.com>
2025-05-16 17:49:49 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <SidebarInset className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14 print:p-0 print:m-0 print:sm:pl-0">
    
    with the text content:
    
        Toggle SidebarClarityFinanceToggle user menuDashboardYour financial overview.Total Spent (This Month)$413.50Sum of all transactions this month.Budget Remaining$586.50Total budget: $1000.00Avg. Daily Spending$25.84Average spending per day this month.Total Transactions7All recorded transactions.Recent TransactionsYour latest financial activities.View AllVendorCategoryDateAmountUnknown Vendor5/16/2025 - OtherOther5/16/2025$25.00SuperMart5/15/2025 - GroceriesGroceries5/15/2025$75.50Edison Electric5/14/2025 - UtilitiesUtilities5/14/2025$120.00City Gas5/13/2025 - UtilitiesUtilities5/13/2025$45.20Metro Transit5/11/2025 - TransportationTransportation5/11/2025$50.00Spending by CategoryYour spending distribution across categories.
         [data-chart=chart-«r0»] {
          --color-Utilities: hsl(var(--chart-3));
          --color-Groceries: hsl(var(--chart-2));
          --color-Dining Out: hsl(20, 80%, 60%);
          --color-Transportation: hsl(var(--chart-4));
          --color-Entertainment: hsl(var(--chart-5));
          --color-Other: hsl(var(--chart-1));
        }
    
        .dark [data-chart=chart-«r0»] {
          --color-Utilities: hsl(var(--chart-3));
          --color-Groceries: hsl(var(--chart-2));
          --color-Dining Out: hsl(20, 80%, 60%);
          --color-Transportation: hsl(var(--chart-4));
          --color-Entertainment: hsl(var(--chart-5));
          --color-Other: hsl(var(--chart-1));
        }
        Utilities ($165.20)Groceries ($75.50)Dining Out ($65.80)Transportation ($50.00)Entertainment ($32.00)Other ($25.00)
    
    Relevant files:
    
        - https://6000-firebase-studio-1747415821302.cluster-xpmcxs2fjnhg6xvn446ubtgpio.cloudworkstations.dev/_next/static/chunks/src_7d628a7b._.js
        - https://6000-firebase-studio-1747415821302.cluster-xpmcxs2fjnhg6xvn446ubtgpio.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    no top margin given

--- src/components/layout/AppLayoutClient.tsx
+++ src/components/layout/AppLayoutClient.tsx
@@ -10,7 +10,7 @@ export function AppLayoutClient({ children }: { children: ReactNode }) {
     <SidebarProvider defaultOpen={false}>
       <div className="flex min-h-screen w-full flex-col bg-muted/40">
         <AppSidebar />
-        <SidebarInset className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14 print:p-0 print:m-0 print:sm:pl-0">
+        <SidebarInset className="flex flex-col sm:gap-4 sm:pb-4 sm:pl-14 print:p-0 print:m-0 print:sm:pl-0">
           <AppHeader />
           <main className="flex-1 gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 print:p-0">
             {children}
